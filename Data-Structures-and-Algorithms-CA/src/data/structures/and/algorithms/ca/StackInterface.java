32999bfb9eb76c4e27b07ba2fc5ff54214f0dff3
AntonioZiska <antonioziska@gmail.com>
2026-03-18 17:29:11 +0000

    StackInterface

---
+++ Data-Structures-and-Algorithms-CA/src/data/structures/and/algorithms/ca/StackInterface.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package data.structures.and.algorithms.ca;
+
+/**
+ *
+ * @author anton
+ */
+public interface StackInterface {
+
+    public Object pop();
+
+    public void emptyStack();
+
+    public String displayStack();
+
+    public void push(Object e);
+
+    public Object peek();
+
+    public int size();
+
+    public boolean isEmpty();
+
+}
