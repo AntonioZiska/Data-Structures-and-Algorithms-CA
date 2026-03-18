651e6ec3e2bf34a8f1e0fe0bf5fb405560e1ae0d
AntonioZiska <antonioziska@gmail.com>
2026-03-18 17:05:59 +0000

    main

---
+++ Data-Structures-and-Algorithms-CA/src/data/structures/and/algorithms/ca/caAPP.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package data.structures.and.algorithms.ca;
+
+/**
+ *
+ * @author anton
+ */
+public class caAPP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        myGUI mygui = new myGUI();
+        mygui.setVisible(true);
+
+    }
+
+}
