a4cb5b542348275b29dae0e40ae077cc19616fd9
AntonioZiska <antonioziska@gmail.com>
2026-03-18 17:51:08 +0000

    trash
    added trash object specifically

--- Data-Structures-and-Algorithms-CA/src/data/structures/and/algorithms/ca/trash.java
+++ Data-Structures-and-Algorithms-CA/src/data/structures/and/algorithms/ca/trash.java
@@ -4,73 +4,49 @@
  */
 package data.structures.and.algorithms.ca;
 
-import java.util.*;
-
 /**
  *
  * @author anton
  */
-public class trash implements StackInterface {
+public class trash {
 
-    private final ArrayList<String> stack;
+    private String name;
+    private int quantity;
+    private int value;
 
-    public trash(ArrayList stack) {
-        this.stack = stack;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
     }
 
     @Override
-    public Object pop() {
-        if (!(stack.isEmpty())) {
-            return stack.remove(0);
-        } else {
-            return null;
-        }
+    public String toString() {
+        return "trash{" + "name=" + name + ", quantity=" + quantity + ", value=" + value + '}';
     }
 
-    @Override
-    public void emptyStack() {
-        while (!stack.isEmpty()) {
-            pop();
-        }
-    }
-
-    @Override
-    public String displayStack() {
-        int iCount;
-        String Message = "";
-        if (stack.isEmpty()) {
-            Message = Message.concat("The stack is empty, there is nothing to display.");
-        } else {
-            Message = "The stack contains: ";
-            for (iCount = 0; iCount < stack.size(); iCount++) {
-                Message = Message.concat(stack.get(iCount) + ", ");
-            }
-        }
-        return Message;
-    }
-
-    @Override
-    public void push(Object newelement) {
-        stack.add(0, (String) newelement);
-    }
-
-    @Override
-    public Object peek() {
-        if (!(stack.isEmpty())) {
-            return stack.get(0);
-        } else {
-            return null;
-        }
-    }
-
-    @Override
-    public int size() {
-        return stack.size();
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return stack.isEmpty();
+    public trash(String name, int quantity, int value) {
+        this.name = name;
+        this.quantity = quantity;
+        this.value = value;
     }
 
 }
